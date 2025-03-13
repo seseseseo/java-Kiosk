dee44b3ab2fa8f2885e2f3af904c72fe5a195991
hanna <leehanna0720@gmail.com>
2025-03-13 17:00:55 +0900

    feat: Lv2. 객체 지향 설계를 적용해 햄버거 메뉴를 클래스로 관리하기
    BREAKING CHANGE:
    - MenuItem 클래스 생성
    - List로 Menuitem 관리
    - 반복문으로 List 접근
    - 선택한 메뉴 출력

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,40 @@
+import example.kiosk.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        List<MenuItem> menuItems; // 메뉴 아이템 리스트 1.쉑쉑버거 2. 가격 3. 설명
         Scanner scanner = new Scanner(System.in);
         int choice;
 
+
+        //List 선언 후 메뉴 추가
+        menuItems = new ArrayList<MenuItem>();
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
         while (true) {
             System.out.println("[ SHAKESHACK MENU ]");
-            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-            System.out.println("0. 종료      | 종료");
-
+            for(int i=0; i< menuItems.size(); i++){
+                System.out.println((i+1) + ". "+ menuItems.get(i).toString());
+            }
+            System.out.println("0. 종료          | 종료");
+            System.out.print("메뉴 번호를 선택하세요: ");
             choice = scanner.nextInt();
 
             if (choice == 0) {
                 System.out.println("\n프로그램을 종료합니다.");
                 break;
+            }else if (choice > 0 && choice <= menuItems.size()) {
+                System.out.println("선택한 메뉴는: " + menuItems.get(choice-1).toString());
+                System.out.println();
             }
         }
 
---
+++ src/example/kiosk/MenuItem.java
@@ -0,0 +1,28 @@
+package example.kiosk;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s | W %.1f | %s", name, price, description);
+    }
+
+}
